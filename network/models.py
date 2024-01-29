105822894b453ae7a10cce9a6f2bd61f12727572
Joe Lambon <joe.lambon25@gmail.com>
2024-01-29 09:37:05 -0800

    Added likes to Post view

--- network/models.py
+++ network/models.py
@@ -11,4 +11,21 @@ def validate_positive(value):
 # Define this to help retrieve User's username in serialization
 class UserManager(models.Manager):
     def get_by_natural_key(self, username):
-     
+        return self.get(username=username)
+
+class User(AbstractUser):
+    def natural_key(self):
+        return (self.username)
+    pass
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    body = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
+    likes = models.IntegerField(default=0, validators=[validate_positive])
+
+    # Format DateTime object as desired
+    def timestamp(self):
+        return self.created.strftime('%d/%m/%y %I:%M %p')
+        
