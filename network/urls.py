e139ed74004c6725957c46c3e0f1e12d29f35b69
Joe Lambon <joe.lambon25@gmail.com>
2024-01-30 19:44:14 -0800

    Updated views to accomodate profile.html

---
+++ network/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
+    path("new_post/<int:user_id>/", views.new_post, name=
