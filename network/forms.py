66f55db053652ccd7dac7d4259a89bfe61d22399
Joe Lambon <joe.lambon25@gmail.com>
2024-01-30 19:37:06 -0800

    Updated views to accomodate profile.html

--- network/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class NewPostForm(forms.Form):
-    title = forms.CharField(max_length=64, required=True)
-    body = forms.CharField(widget=forms.Textarea(attrs={'name':'body',
-                                 'style': 'height: 3em;'}),max_length=255, required=True)
-    
